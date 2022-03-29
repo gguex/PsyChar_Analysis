09d4da8d590ed320874e16ff9ed703f539755c67
semanticboy <davide.picca@unil.ch>
2022-03-29 16:43:29 +0200

    Add script and results

---
+++ 1.2_preprocess_kaggle_mbti.py
@@ -0,0 +1,19 @@
+import pandas as pd
+#read dataset
+mbti=pd.read_csv('corpora/kaggle_mbti/mbti_1.csv')
+
+#clean dataset
+mbti['posts']=mbti['posts'].str.replace('\|\|\|',' ')
+
+#group by personality type
+types=mbti.groupby('type')['posts'].apply(lambda x: '\n'.join(x)).reset_index()
+
+#write a file for each personality type
+for row in types.iterrows():
+    print(row[1]['type'])
+    with open(f"corpora/kaggle_mbit/output/{row[1]['type']}.txt",'w') as file:
+        file.write(row[1]['posts'])
+
+
+
+
